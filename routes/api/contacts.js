30edb12e5c7be508ff0bd4aeb51c7a4d45bae4e6
Michael Paukov <michael.paukov@gmail.com>
2022-03-31 15:46:46 +0200

    HW03 Done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,13 @@
 const express = require("express");
 const CreateError = require("http-errors");
-const Joi = require("joi");
 
-const contactsOperations = require("../../models/contacts");
-
-const contactSchema = Joi.object({
-  name: Joi.string()
-    .min(3)
-    .pattern(/^[a-zA-Z ]+$/)
-    .required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string()
-    .pattern(/^[0-9]+-[0-9]+-[0-9]+$/, "numbers")
-    .required(),
-});
+const { Contact, schemas } = require("../../models/contact");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const contacts = await contactsOperations.listContacts();
+    const contacts = await Contact.find({}, "-createdAt -updatedAt");
     res.json(contacts);
   } catch (error) {
     next(error);
@@ -29,26 +17,36 @@ router.get("/", async (req, res, next) => {
 router.get("/:contactId", async (req, res, next) => {
   try {
     const { contactId: id } = req.params;
-    const contact = await contactsOperations.getContactById(id);
+    const contact = await Contact.findById(id, "-createdAt -updatedAt");
+
     if (!contact) {
       throw new CreateError(404, "Not found");
     }
     res.json(contact);
   } catch (error) {
+    if (error.message.includes("ObjectId failed for value")) {
+      error.status = 404;
+      error.message = "Not found";
+    }
     next(error);
   }
 });
 
 router.post("/", async (req, res, next) => {
   try {
-    const { error } = contactSchema.validate(req.body);
+    const { error } = schemas.add.validate(req.body);
     if (error) {
       throw new CreateError(400, error.message);
     }
-    const body = req.body;
-    const newContact = await contactsOperations.addContact(body);
+    const newContact = await Contact.create(req.body);
     res.status(201).json(newContact);
   } catch (error) {
+    if (
+      error.message.includes("validation failed") ||
+      error.message.includes("duplicate key error")
+    ) {
+      error.status = 400;
+    }
     next(error);
   }
 });
@@ -56,7 +54,7 @@ router.post("/", async (req, res, next) => {
 router.delete("/:contactId", async (req, res, next) => {
   try {
     const { contactId: id } = req.params;
-    const contact = await contactsOperations.removeContact(id);
+    const contact = await Contact.findByIdAndDelete(id);
     if (!contact) {
       throw new CreateError(404, "Not found");
     }
@@ -68,18 +66,43 @@ router.delete("/:contactId", async (req, res, next) => {
 
 router.put("/:contactId", async (req, res, next) => {
   try {
-    const { error } = contactSchema.validate(req.body);
+    const { error } = schemas.add.validate(req.body);
     if (error) {
       throw new CreateError(400, error.message);
     }
-    const body = req.body;
     const { contactId: id } = req.params;
-    const contact = await contactsOperations.updateContact(id, body);
+    const contact = await Contact.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
     if (!contact) {
       throw new CreateError(404, "Not found");
     }
     res.json(contact);
   } catch (error) {
+    next(error);
+  }
+});
+
+router.patch("/:contactId/favorite", async (req, res, next) => {
+  try {
+    const { error } = schemas.updateFavorite.validate(req.body);
+    if (error) {
+      throw new CreateError(400, "Missing field favorite");
+    }
+
+    const { contactId: id } = req.params;
+    const contact = await Contact.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!contact) {
+      throw new CreateError(404, "Not found");
+    }
+    res.json(contact);
+  } catch (error) {
+    if (error.message.includes("ObjectId failed for value")) {
+      error.status = 404;
+      error.message = "Not found";
+    }
     next(error);
   }
 });
